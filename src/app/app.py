92e1b649c8ceee6517aa29fdffc15af322d7cede
Samay Singh Bisht <samay.samay987@gmail.com>
2023-10-02 00:13:58 +0200

    fixing some last minute bugs

--- src/app/app.py
+++ src/app/app.py
@@ -7,15 +7,14 @@ app = Flask(__name__)
 minio_host = os.getenv("MINIO_HOST") 
 # Initialize the Minio client
 minio_client = Minio(
-    minio_host,  # Replace with the Minio service DNS or IP
-    access_key=os.getenv("MINIO_ACCESS_KEY"),  # Replace with your Minio access key
-    secret_key=os.getenv("MINIO_SECRET_KEY"),  # Replace with your Minio secret key
-    secure=False,  # Set to True if your Minio server uses HTTPS
+    minio_host,  
+    access_key=os.getenv("MINIO_ACCESS_KEY"), 
+    secret_key=os.getenv("MINIO_SECRET_KEY"), 
+    secure=False,  
 )
 
 @app.route("/")
 def index():
-    # Replace 'justwatch' with your Minio bucket name
     bucket_name = "justwatch"
 
     # List objects in the bucket
--- src/upload/upload.py
+++ src/upload/upload.py
@@ -1,50 +1,17 @@
-# import os
-# from minio import Minio
-# # from minio.error import ResponseError
-
-# # MinIO server configuration
-# minio_host = "minio.minio.svc.cluster.local"  # DNS name of the MinIO service in infra cluster
-# minio_access_key = os.getenv("minio")
-# minio_secret_key = os.getenv("minioadmin")
-
-# # Create a MinIO client
-# minio_client = Minio(
-#     minio_host,
-#     access_key=minio_access_key,
-#     secret_key=minio_secret_key,
-#     secure=False,  # Set to True if MinIO server uses HTTPS
-# )
-
-# def create_bucket(bucket_name):
-#     # try:
-#         if not minio_client.bucket_exists(bucket_name):
-#             minio_client.make_bucket(bucket_name)
-#             print(f"Bucket '{bucket_name}' created successfully.")
-#         else:
-#             print(f"Bucket '{bucket_name}' already exists.")
-#     # except ResponseError as err:
-#     #     print(f"Error: {err}")
-
-# if __name__ == "__main__":
-#     bucket_name = "justwatch"
-#     create_bucket(bucket_name)
-
 import os
 from minio import Minio
 from minio.error import S3Error
-#from pprint import pprint
+
 
 # MinIO server configuration
 minio_host = os.getenv("MINIO_HOST")  # DNS name of the MinIO service in infra cluster
-# minio_access_key = os.getenv("minio")
-# minio_secret_key = os.getenv("minio123")
 
 # Create a MinIO client
 minio_client = Minio(
     minio_host,
     access_key=os.getenv("MINIO_ACCESS_KEY"),
     secret_key=os.getenv("MINIO_SECRET_KEY"),
-    secure=False,  # Set to True if MinIO server uses HTTPS
+    secure=False,
 )
 
 def create_bucket(bucket_name):
@@ -57,17 +24,15 @@ def create_bucket(bucket_name):
     except S3Error as err:
          print(f"Error: {err}")
 
+
 # Upload images from the local directory to the Minio bucket
 def upload_images(bucket_name):
     try:
         for filename in os.listdir("images"):
-            print("folder")
             if filename.endswith((".jpeg", ".webp")):
-                print("found the images")
                 object_name = filename
                 file_path = os.path.join("images", filename)
                 minio_client.fput_object(bucket_name, object_name, file_path)
-                print("Images Pushed")
         return True
     except S3Error as err:
         print(err)
